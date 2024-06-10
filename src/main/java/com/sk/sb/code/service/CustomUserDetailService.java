bff518da9fc485b2c692107a7e38781dfe857e35
Santosh-97GithubRepo <164603680+Santosh-97GithubRepo@users.noreply.github.com>
2024-06-10 09:18:26 +0530

    Create a custom UserDetailsService to load user-specific data

--- src/main/java/com/sk/sb/code/service/CustomUserDetailService.java
+++ src/main/java/com/sk/sb/code/service/CustomUserDetailService.java
@@ -1,15 +1,40 @@
 package com.sk.sb.code.service;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.sk.sb.code.entity.Role;
+import com.sk.sb.code.entity.User;
+import com.sk.sb.code.repository.UserRepository;
+
 public class CustomUserDetailService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByUserName(username)
+		         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+		
+		return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+	}
+
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
 		// TODO Auto-generated method stub
-		return null;
+		return roles.stream()
+				.map(role-> new SimpleGrantedAuthority(role.getName()))
+				.collect(Collectors.toList());
 	}
 
 }
