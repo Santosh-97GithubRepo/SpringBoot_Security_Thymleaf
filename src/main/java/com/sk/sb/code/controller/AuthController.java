f8105b02a4562773f3c64a5604ac85d69df3cdd9
Santosh-97GithubRepo <164603680+Santosh-97GithubRepo@users.noreply.github.com>
2024-06-11 20:55:23 +0530

    AuthController to handle authentication

---
+++ src/main/java/com/sk/sb/code/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.sk.sb.code.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+}
